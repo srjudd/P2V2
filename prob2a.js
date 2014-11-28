bfa4c1d268fb616e55b262af9d1d244dfe8fa3bd
Renee Judd <rjudd@nl.edu>
2014-11-28 09:44:33 -0600

    First prob restructured to use inner function. Comments were added.

--- prob2a.js
+++ prob2a.js
@@ -1,25 +1,43 @@
 function getRelationship(x, y) {
-	var	NaNct = 0;
-	var subject = "";
-	if (isNaN(x)) {
-		NaNct++;
-		subject = x;
-	}
-	if (isNaN(y)) {
-		NaNct++;
-		if (NaNct > 1) {subject = subject + " and " + y;}
-			else {subject = y;}
-	}
-	if (NaNct === 0) {
-		if (x === y) {return "=";}
-		if (x > y) {return ">";}
-		if (x < y) {return "<";}
-	}
-	else {
-		var compop = "Can\'t compare relationships because ";
-		var verb = "";
-		if (NaNct === 1) {verb = " is not a number";}
-		else {verb = " are not numbers";}
-		return compop + subject + verb;
-	}
+    var NaNct = 0;
+    var subjectArray = [];
+
+    //getRelationship compares two numbers and returns the symbol
+    //representing their relationship, or gives an error message if the inputs are
+    //not two numbers.
+
+    //The myNaNct function checks for inputs that are not numbers, counts them,
+    //and puts them into an array for an error message.
+
+    function myNaNct(z) {
+            if (isNaN(z)) {
+                NaNct++;
+                subjectArray.push(z);
+            }
+        } //end myNanct
+
+    myNaNct(x);
+    myNaNct(y);
+
+    if (NaNct === 0) {
+        if (x === y) {
+            return "=";
+        } else {
+            if (x > y) {
+                return ">";
+            } else {
+                return "<";
+            }
+        }
+    }
+
+    //function ends if NaNct is 0, else givea error message
+    else {
+        if (NaNct === 1) {
+            return "Can\'t compare relationships because " + subjectArray[0] + " is not a number";
+        } else {
+            return "Can\'t compare relationships because " + subjectArray[0] + " and " + subjectArray[1] + " are not numbers";
+        }
+
+    }
 }
